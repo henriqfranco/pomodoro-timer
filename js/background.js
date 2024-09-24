1c8acf45f2a079ecf250a2465fb68407ce1417a8
Henrique Franco <henriqfvr@gmail.com>
2024-09-24 15:53:31 -0300

    background gifs fixed

--- js/background.js
+++ js/background.js
@@ -4,11 +4,11 @@ const main = document.querySelector('main');
 
 function applyBackground(selectedBg) {
     if (selectedBg === 'your-name') {
-        body.style.backgroundImage = 'url(/imgs/f5f27448c036af645c27467c789ad759.gif)';
+        body.style.backgroundImage = 'url(./imgs/f5f27448c036af645c27467c789ad759.gif)';
         main.style.background = 'rgba(48, 55, 216, 0.39)';
         main.style.border = '1px solid rgba(48, 55, 216, 1)';
     } else if (selectedBg === 'anime-girl') {
-        body.style.backgroundImage = 'url(/imgs/6bjrT2y.gif)';
+        body.style.backgroundImage = 'url(./imgs/6bjrT2y.gif)';
         main.style.background = 'rgba(245, 197, 63, 0.37)';
         main.style.border = '1px solid rgba(245, 197, 63, 1)';
     } else if (selectedBg === 'tongue'){
@@ -16,7 +16,7 @@ function applyBackground(selectedBg) {
         main.style.background = 'rgba(243, 245, 63, 0.37)';
         main.style.border = '1px solid rgba(243, 245, 63, 1)';
     } else if (selectedBg === 'surprise'){
-        body.style.backgroundImage = 'url(/imgs/f91669844da772dc0c00e9f627d2fd7e.gif)';
+        body.style.backgroundImage = 'url(./imgs/f91669844da772dc0c00e9f627d2fd7e.gif)';
         main.style.background = 'rgba(237, 117, 239, 0.37)';
         main.style.border = '1px solid rgba(237, 117, 239, 1)';
     }
